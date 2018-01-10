80442f923972057e93ac9bf9e12381eb98bd842f
Nikolay Semernya <nsemernia@gmail.com>
2018-01-10 23:58:15 +0700

    Add title validation and test for it

--- src/components/course/CourseForm.Enzyme.test.js
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -17,7 +17,7 @@ function setup(saving) {
 it('renders form and h1', () => {
   const wrapper = setup(false);
   expect(wrapper.find('form').length).toBe(1);
-  expect(wrapper.find('h1').text()).toEqual('Manage Course')
+  expect(wrapper.find('h1').text()).toEqual('Manage Course');
 });
 
 it('save button is labeled "Save" when not saving', () => {
